c171bd4113d87fb21c7679106d86a64384eb6ad2
qingwei <l.q.wei91@gmail.com>
2014-12-14 22:20:50 +0800

    code completed

--- webserver.js
+++ webserver.js
@@ -1,6 +1,8 @@
 //===============Setting up HTTP server to server html================
 var http = require('http');
 var fs = require('fs');
+//var Galileo = require("galileo-io");
+//var board = new Galileo();
 
 var html = fs.readFileSync('index.html');
 http.createServer(function (req, res) {
@@ -22,17 +24,25 @@ var Firebase = require('firebase');
 var tempDataStore = new Firebase('https://gen-homework.firebaseio.com/');
 tempDataStore = tempDataStore.child('server-temperature').child(process.argv[2]);
 var moment = require('moment-timezone');
+var degreecelsius;
 
 setInterval(function(){
 	var temp = getCurrentTemp();
 	//var time = moment.tz('Asia/Kuala_Lumpur').format('HH:mm:ss');
 	var time = Date.now();
+	//var temp = degreecelsius;
 	updateDatabase(tempDataStore,temp,time);
-},20*1000);
+},1000);
+
+// board.analogRead("A0", function(data) {
+//   //console.log(data* 0.48828125);
+//     degreecelsius = data;
+// });
 
 function getCurrentTemp(){
 	return ((100*Math.random()%7) + 30);
 }
+
 function updateDatabase(dataStore,currentTemp,currentDate){
 	dataStore.child(currentDate).set(
 			{"Temperature":currentTemp}
